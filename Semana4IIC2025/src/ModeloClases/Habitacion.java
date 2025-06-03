510e94b0d333d3c4f588baedf6eeea5df14d3166
stevenarias65 <44099803+stevenarias65@users.noreply.github.com>
2025-06-02 21:53:45 -0600

    Semana 4

---
+++ Semana4IIC2025/src/ModeloClases/Habitacion.java
@@ -0,0 +1,55 @@
+package ModeloClases;
+
+public class Habitacion {
+/*
+    Cada clase debe de tener atributos y metodos
+    
+    Declaramos Atributos
+    No es solo declarar por declarar, cada objeto va a poder utilizar estos atributos
+    */
+    //Sistema para reservar habitaciones de hotel 
+    //ATRIBUTO 
+    private double tamaño; 
+    private int numeroHabitacion;
+    private String tipoHabitacion;
+    private int capacidadPersonas; 
+    private double precio;
+    private boolean estado; 
+    
+    
+    //Crear los metodos GET y SET
+    //cuando decimos GET srive para obtener la informacion
+    //cuando es SET establecemos la informacion 
+    private double getTamaño(){
+        return tamaño;
+    }
+    
+    public void setTamaño(  double tamaño  ){
+        if (tamaño < 0) {
+            System.out.println("No se permite numeros negativos");
+        }else{
+            this.tamaño = tamaño;
+        }      
+    }
+    
+    public int getNumeroHabitacion(){
+        return numeroHabitacion;
+    }
+    
+    public void setNumeroHabitacion(int numeroHabitacion){
+        this.numeroHabitacion = numeroHabitacion;
+    }
+    
+    
+    //Crear un metodo que me muestre la habitacion 
+    //encapsulamiento + tipo + nombre + parametros
+    //public
+    //private
+    //protected
+    //default
+    //Tipos: void,String,int o cual tipo de datos privitivo
+    public void mostrarInformacion(){
+        System.out.println("Estoy en la habitacion: " + numeroHabitacion );
+    }
+    
+}
---
+++ Semana4IIC2025/src/ModeloClases/Inicio.java
@@ -0,0 +1,41 @@
+
+package ModeloClases;
+
+
+public class Inicio {
+        
+    public static void main(String[] args) {
+        
+        //Objeto 
+        Habitacion habitacion1 = new Habitacion(); 
+        Habitacion habitacion2 = new Habitacion(); 
+        Habitacion habitacion3 = new Habitacion(); 
+        Habitacion habitacion4 = new Habitacion(); 
+        Habitacion habitacion5 = new Habitacion(); 
+        Habitacion habitacion6 = new Habitacion(); 
+        Habitacion habitacion7 = new Habitacion(); 
+        Habitacion habitacion8 = new Habitacion(); 
+        Habitacion habitacion9 = new Habitacion(); 
+        Habitacion habitacion10 = new Habitacion(); 
+        Habitacion habitacion11 = new Habitacion(); 
+        Habitacion habitacion12 = new Habitacion(); 
+        Habitacion habitacion13 = new Habitacion(); 
+        Habitacion habitacion14 = new Habitacion(); 
+        Habitacion habitacion15 = new Habitacion(); 
+        Habitacion habitacion16 = new Habitacion(); 
+        
+        
+        habitacion1.setNumeroHabitacion(101);
+       int retornoInformacion =  habitacion1.getNumeroHabitacion();
+        System.out.println(habitacion1.getNumeroHabitacion());
+        
+     
+        
+        
+        
+        
+    }
+    
+    
+    
+}
